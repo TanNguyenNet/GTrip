68d352fb6a985ef8f3e881ed06a2213be44307b9
TanNguyen <itnbtan@gmail.com>
2017-12-05 18:03:48 +0700

    Config DB

---
+++ NBT/NBT.Core.Datas/Migrations/Configuration.cs
@@ -0,0 +1,51 @@
+namespace NBT.Core.Datas.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using NBT.Core.Domain.Identity;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    public sealed class Configuration : DbMigrationsConfiguration<NBT.Core.Datas.MasterDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(NBT.Core.Datas.MasterDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+            InitUser(context);
+        }
+
+        private void InitUser(MasterDBContext context)
+        {
+            var manager = new UserManager<AppUser>(new UserStore<AppUser>(new MasterDBContext()));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new MasterDBContext()));
+            if (manager.Users.Count() != 0)
+                return;
+            var user = new AppUser()
+            {
+                UserName = "admin",
+                Email = "gtrip@gmail.com",
+                EmailConfirmed = true
+
+            };
+
+            manager.Create(user, "123456");
+
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
+        }
+    }
+}
---
+++ NBT/NBT.Web.Api/Controllers/Identity/AppGroupController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NBT.Core.Services.ApplicationServices.System;
+
+namespace NBT.Web.Api.Controllers.Identity
+{
+    public class AppGroupController : BaseApiController
+    {
+
+        public AppGroupController(IErrorService errorService) : base(errorService)
+        {
+
+        }
+    }
+}
