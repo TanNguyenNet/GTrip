076d74c42ff7ae34524dc7d338becb944b9c7a83
tannb <itnbtan@gmail.com>
2018-10-09 15:32:59 +0700

    add page fight

---
+++ NBT/NBT.Web/Controllers/FightController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NBT.Web.Controllers
+{
+    public class FightController : Controller
+    {
+        // GET: Fight
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
